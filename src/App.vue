04f0ddd76037bfad2042247e9c050713e30f812c
Robin van de Griend <robinvandegriend@gmail.com>
2023-07-22 19:56:53 +0200

    Make content scroll in its own scroll container

--- src/App.vue
+++ src/App.vue
@@ -33,31 +33,29 @@ useRouter().beforeEach((to) => {
 <template>
   <div
     :style="{ 'margin-bottom': overlayHeight + 'px' }"
-    class="grid-cols-[1fr_3fr] grid-rows-[min-content_1fr] gap-x-4 px-4 leading-tight text-dark-green lg:grid lg:max-h-screen lg:grid-cols-4 lg:p-6 lg:pb-0"
+    class="grid-cols-[1fr_3fr] grid-rows-[min-content_1fr] gap-x-4 p-4 pt-2 leading-tight text-dark-green lg:grid lg:max-h-screen lg:grid-cols-4 lg:p-6 lg:pb-0"
   >
     <MenuButton
-      class="ml-auto mt-2 w-10 lg:hidden"
+      class="ml-auto w-10 lg:hidden"
       @click="toggleMobileMenu()"
       :show-cross="mobileMenuShown"
     />
     <TheHeader class="col-span-full lg:mb-6" />
     <TheMenu v-show="mobileMenuShown || lgBreakpoint" class="mt-10 flex lg:mt-0" />
     <RouterView
-      class="col-span-2 mb-4 mt-10 lg:my-0 lg:overflow-y-auto"
+      class="lg:scroll-stable col-span-2 mb-4 mt-10 lg:my-0 lg:overflow-y-auto"
       v-show="!mobileMenuShown || lgBreakpoint"
     />
+    <RouterLink to="/" v-show="!mobileMenuShown" class="lg:hidden">
+      <TheLogo class="mt-10 h-32" />
+    </RouterLink>
+
     <div class="fixed bottom-0 right-0 mb-20 hidden w-1/4 items-end justify-end pr-6 lg:flex">
       <TheInfo />
       <RouterLink class="shrink-0" to="/">
         <TheLogo class="ml-3 h-48" />
       </RouterLink>
     </div>
-  </div>
-
-  <div ref="overlay" class="fixed bottom-0 flex w-full flex-auto items-end lg:hidden">
-    <RouterLink v-if="!hideLogo" to="/" class="m-4 block h-32">
-      <TheLogo class="h-full" />
-    </RouterLink>
   </div>
 </template>
 
