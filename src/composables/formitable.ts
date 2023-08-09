0fb1352b3e6fbe7ad0d0363ed5e5ea86b4524b40
Robin van de Griend <robinvandegriend@gmail.com>
2023-08-09 13:32:51 +0200

    Wait for widget to be ready

--- src/composables/formitable.ts
+++ src/composables/formitable.ts
@@ -3,16 +3,33 @@ import { ref, watchEffect } from 'vue'
 const widgetShown = ref(false)
 
 export function useFormitable() {
+  const widgetReady = ref(false)
+
+  // @ts-ignore
+  if (window.formitableWidgetReady) {
+    // Widget is already loaded
+    console.log('FT WIDGET READY')
+    widgetReady.value = true
+  } else {
+    // Widget is not yet loaded, so we set an eventlistener to wait for it
+    window.addEventListener('ft-widget-ready ', () => {
+      console.log('FT WIDGET READY EVENT FIRED')
+      widgetReady.value = true
+    })
+  }
+
   function showWidget() {
-    (FT.widgets.get().element as HTMLElement).style.cssText = ''
+    ;(FT.widgets.get().element as HTMLElement).style.cssText = ''
   }
 
   function hideWidget() {
-    (FT.widgets.get().element as HTMLElement).style.cssText = 'display: none !important;'
+    ;(FT.widgets.get().element as HTMLElement).style.cssText = 'display: none !important;'
   }
 
   watchEffect(() => {
-    widgetShown.value ? showWidget() : hideWidget()
+    if (widgetReady.value) {
+      widgetShown.value ? showWidget() : hideWidget()
+    }
   })
 
   return { widgetShown }
