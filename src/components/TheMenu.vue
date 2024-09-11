151e1f39b2384dcf5f2904f364f18ec475fba0d9
Robin van de Griend <robinvandegriend@gmail.com>
2024-09-11 11:46:28 +0200

    Add private hire and family navigation item

--- src/components/TheMenu.vue
+++ src/components/TheMenu.vue
@@ -13,6 +13,8 @@ import MenuItem from './MenuItem.vue'
     <MenuItem to="/wines">wines</MenuItem>
     <MenuItem to="/jobs">jobs</MenuItem>
     <MenuItem to="/events">events</MenuItem>
+    <MenuItem to="/private-hire">private hire</MenuItem>
     <MenuItem to="/gallery">gallery</MenuItem>
+    <MenuItem to="/family">family</MenuItem>
   </nav>
 </template>
