13857dbe262493038cf89e54fc57f4b768bafaf7
Robin van de Griend <robinvandegriend@gmail.com>
2025-11-20 14:20:08 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh root@goldie.robinvdgriend.nl "cd /srv/binnenvisser; git pull;docker-compose pull;docker-compose build; docker-compose up -d;"
+ssh root@goldie.robinvdgriend.nl "cd /srv/binnenvisser; git pull;docker compose pull;docker compose build; docker compose up -d;"
